8402f5ae3a670dd2fe7c87e30252bb6522be3fe2
Michiaki Furusho <m.furusho@gmail.com>
2023-10-06 23:09:18 +0900

    32章コード追加

---
+++ src/032.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+error_reporting(-1);
+
+// 簡単な無名クラス
+$object = new class() {
+    public function test()
+    {
+        var_dump(__METHOD__);
+    }
+};
+$object->test();
+
+// コンストラクタを持つ無名クラス
+$object = new class(123, 'str', [1, 2, 3]) {
+    public function __construct(
+        private int $num,
+        private string $str,
+        private array $arr,
+    ) {
+    }
+};
+var_dump($object);
+
+// 継承
+$object = new class() extends arrayObject {
+};
+var_dump($object instanceof arrayObject);
+
+// interfaceの継承
+$object = new class() implements Countable {
+    public function count(): int
+    {
+        return 0;
+    }
+};
+var_dump($object instanceof Countable);
+
+// trait
+trait HogeTrait {
+    public function hoge()
+    {
+        var_dump(__METHOD__);
+    }
+}
+$object = new class() {
+    use HogeTrait;
+};
+$object->hoge();
